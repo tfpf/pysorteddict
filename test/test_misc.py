14e1c4d6b9fff93e6ec674d832ce9258f4457b78
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-11-17 19:16:36 +0530

    Concise miscellaneous test

--- test/test_misc.py
+++ test/test_misc.py
@@ -14,57 +14,7 @@ class TestMisc(unittest.TestCase):
             SortedDict()
         self.assertEqual(ctx.exception.args[0], self.missing_argument)
 
-    def test_construct_with_bool_instance(self):
+    def test_construct_with_object_instance(self):
         with self.assertRaises(ValueError) as ctx:
-            SortedDict(False)
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_bytes_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(b"")
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_bytearray_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(bytearray())
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_complex_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(0j)
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_float_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(0.0)
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_int_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(0)
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_list_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict([])
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_memoryview_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(memoryview(b""))
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_range_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(range(0))
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_str_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict("")
-        self.assertEqual(ctx.exception.args[0], self.wrong_argument)
-
-    def test_construct_with_tuple_instance(self):
-        with self.assertRaises(ValueError) as ctx:
-            SortedDict(())
+            SortedDict(object())
         self.assertEqual(ctx.exception.args[0], self.wrong_argument)
