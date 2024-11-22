7d95b3cd278b1d0cd224867f7c57b3221866c02d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-11-23 00:40:52 +0530

    MSVC C++ standard flag is different

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+import platform
+
 import setuptools
 
 setuptools.setup(
     ext_modules=[
         setuptools.Extension(
             name="pysorteddict",
-            extra_compile_args=["-std=c++11"],
+            extra_compile_args=["/std:c++14" if platform.system() == "Windows" else "-std=c++11"],
             sources=["src/pysorteddict/pysorteddict.cc"],
             py_limited_api=True,
         )
