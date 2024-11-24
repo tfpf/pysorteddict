780b813d22a9aafa71c9caa3eb4a839578b3baeb
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-11-24 13:38:09 +0530

    Detect Cygwin or MSYS2 and set the standard corrrctly

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-import platform
+import os
+import sysconfig
 
 import setuptools
+
+if os.name != "nt" or sysconfig.get_platform().startswith("mingw"):
+    # Either one of Linux, macOS and Cygwin, or MSYS2.
+    cxx_standard_arg = "-std=c++14"
+else:
+    # Windows.
+    cxx_standard_arg = "/std:c++14"
 
 setuptools.setup(
     ext_modules=[
         setuptools.Extension(
             name="pysorteddict",
-            extra_compile_args=["/std:c++14" if platform.system() == "Windows" else "-std=c++14"],
+            extra_compile_args=[cxx_standard_arg],
             sources=["src/pysorteddict/pysorteddict.cc"],
             py_limited_api=True,
         )
