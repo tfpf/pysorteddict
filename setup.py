bbff73cdd6ef2605cddf5b46013ce694aff4d584
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-11-24 14:12:41 +0530

    Source installation fixes for Cygwin and MSYS2 (#16)
    * Detect Cygwin or MSYS2 and set the standard correctly
    * Bumped version

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-import platform
+import os
+import sysconfig
 
 import setuptools
+
+# If running on either one of Linux, macOS and Cygwin, or MSYS2, specify the
+# argument the way the corresponding tools expect it.
+cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++14"
 
 setuptools.setup(
     ext_modules=[
         setuptools.Extension(
             name="pysorteddict",
-            extra_compile_args=["/std:c++14" if platform.system() == "Windows" else "-std=c++14"],
+            extra_compile_args=[cxx_standard_arg],
             sources=["src/pysorteddict/pysorteddict.cc"],
             py_limited_api=True,
         )
