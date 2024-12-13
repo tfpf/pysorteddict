9644b8133049f5cec0ed1bc72415f24d309aecbb
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-12-13 17:03:15 +0530

    Working example of parametrising arguments to a fixture

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,1 +1,15 @@
 import pytest
+
+class Resources:
+    def __init__(self, key_type: type):
+        self.key_type = key_type
+    def __str__(self):
+        return hex(id(self)) + ":" + repr(self.key_type)
+
+@pytest.fixture
+def resources(request):
+    return Resources(request.param)
+
+@pytest.mark.parametrize("resources", [int, float], indirect=True)
+def test_dummy(resources):
+    print(resources)
