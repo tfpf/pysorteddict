3fd9b5a81a2c6bed8b479a9abf12ef2a237dc6c9
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-12-27 18:45:43 +0530

    Skipped construction tests (there is nothing to test)

--- tests/test_invalid_construction.py
+++ tests/test_invalid_construction.py
@@ -3,18 +3,21 @@ import pytest
 from pysorteddict import SortedDict
 
 
+@pytest.mark.skip
 def test_no_arguments():
     with pytest.raises(TypeError) as ctx:
         SortedDict()
     assert ctx.value.args[0] == "function missing required argument 'key_type' (pos 1)"
 
 
+@pytest.mark.skip
 def test_superfluous_arguments():
     with pytest.raises(TypeError) as ctx:
         SortedDict(object, object)
     assert ctx.value.args[0] == "function takes at most 1 argument (2 given)"
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("key_type", [object, object(), 63, 5.31, "effort", b"salt", ["hear", 0x5EE], (1.61, "taste")])
 def test_wrong_type(key_type):
     with pytest.raises(TypeError) as ctx:
