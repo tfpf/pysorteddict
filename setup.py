1c6a559c658f18eff1ae8fb7556d99f40e3be581
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-04-28 19:02:24 +0530

    Use C++20 with MSVC

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ import sysconfig
 
 import setuptools
 
-# If running on either one of Linux, macOS and Cygwin, or MSYS2, specify the
-# argument the way the corresponding tools expect it.
-cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++14"
+# MSVC supports designated initialisers only in C++20.
+cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++20"
 
 setuptools.setup(
     ext_modules=[
