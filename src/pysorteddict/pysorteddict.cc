ee0043f530e67398af9d21855f49145e5f18c974
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-01 16:54:08 +0530

    Cast

--- src/pysorteddict/pysorteddict.cc
+++ src/pysorteddict/pysorteddict.cc
@@ -376,7 +376,7 @@ PyObject* SortedDictType::get_key_type(void)
     {
         Py_RETURN_NONE;
     }
-    return this->key_type;
+    return reinterpret_cast<PyObject*>(this->key_type);
 }
 
 int SortedDictType::init(PyObject* args, PyObject* kwargs)
