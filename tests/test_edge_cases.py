87dff1e0df734391fadcc8d5e78e7948c2bdee84
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-01 15:55:34 +0530

    Added NaN tests

---
+++ tests/test_edge_cases.py
@@ -0,0 +1,20 @@
+import pytest
+
+from pysorteddict import SortedDict
+
+
+def test_bad_keys():
+    bad_keys = [float("nan")]
+    for bad_key in bad_keys:
+        sorted_dict = SortedDict()
+        with pytest.raises(ValueError, match="key type not set: insert at least one item first"):
+            sorted_dict[bad_key]
+        with pytest.raises(ValueError, match=f"bad key: {bad_key}"):
+            sorted_dict[bad_key] = None
+        sorted_dict[type(bad_key)()] = None
+        with pytest.raises(ValueError, match=f"bad key: {bad_key}"):
+            sorted_dict[bad_key] = None
+
+
+if __name__ == "__main__":
+    pytest.main()
