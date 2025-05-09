5e58cbcfb7d4a992300f70d440632f233ef5e22a
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-09 23:36:09 +0530

    Added type-specific random generators
    [skip ci]

--- tests/test_fuzzy.py
+++ tests/test_fuzzy.py
@@ -1,24 +1,41 @@
+import builtins
+import math
 import random
+import string
 
 import pytest
 
 from pysorteddict import SortedDict
 
-rg = random.Random(__name__)
 supported_types = {bytes, int, float, str}
 unsupported_types = {bool, bytearray, complex, dict, frozenset, list, set, tuple}
 
 
 class TestFuzzy:
+    _rg = random.Random(__name__)
+
     def _set_up(self):
-        self.key_type = str
+        self.key_type = self._rg.choice([*supported_types])
         self.normal_dict = {}
         self.sorted_dict = SortedDict()
+
+    def _gen(self):
+        match self.key_type:
+            case builtins.bytes:
+                return self._rg.randbytes(self._rg.randrange(16, 32))
+            case builtins.int:
+                return self._rg.randrange(1_000, 2_000)
+            case builtins.float:
+                return self._rg.choice([f / 1_000 for f in range(1_000)] + [-math.inf, math.inf])
+            case builtins.str:
+                return "".join(self._rg.choices(string.ascii_lowercase, k=self._rg.randrange(20, 30)))
+            case key_type:
+                raise RuntimeError(key_type)
 
     def test_fuzzy(self):
         self._set_up()
 
-        for attr in rg.choices(dir(SortedDict), k=1000000):
+        for attr in self._rg.choices(dir(SortedDict), k=1_000_000):
             match attr:
                 case "__delattr__":
                     self._test_delattr()
