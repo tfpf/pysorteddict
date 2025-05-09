0e0f174e34cf3492572e6f54d48ef80cb67e7dda
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-09 23:13:50 +0530

    Added `delattr` tests
    [skip ci]

--- tests/test_fuzzy.py
+++ tests/test_fuzzy.py
@@ -1,25 +1,42 @@
-import pytest
-from pysorteddict import SortedDict
-from sys import getrefcount
 import random
+
+import pytest
+
+from pysorteddict import SortedDict
 
 rg = random.Random(__name__)
 
-class TestFuzzy:
 
+class TestFuzzy:
     def _set_up(self):
         self.key_type = str
+        self.normal_dict = {}
+        self.sorted_dict = SortedDict()
 
     def test_fuzzy(self):
         self._set_up()
 
-        for attr in rg.choices(dir(SortedDict)):
+        for attr in rg.choices(dir(SortedDict), k=1000000):
             match attr:
+                case "__delattr__":
+                    self._test_delattr()
                 case "__contains__":
                     self._test_contains()
-                case attr:
-                    raise RuntimeError(attr)
+
+    def _test_delattr(self):
+        with pytest.raises(
+            AttributeError, match="^attribute 'key_type' of 'pysorteddict.SortedDict' objects is not writable$"
+        ):
+            delattr(self.sorted_dict, "key_type")
+        for attr in ("clear", "copy", "items", "keys", "values"):
+            with pytest.raises(
+                AttributeError, match=f"^'pysorteddict.SortedDict' object attribute {attr!r} is read-only$"
+            ):
+                delattr(self.sorted_dict, attr)
 
     def _test_contains(self):
         pass
 
+
+if __name__ == "__main__":
+    pytest.main()
