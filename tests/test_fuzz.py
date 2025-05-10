dce25912efecf7ac01828ee274aa513fbdadc46a
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-10 12:41:15 +0530

    Adde back `__setitem__`
    [skip ci]

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -49,8 +49,8 @@ class TestFuzz:
         attrs = {*dir(SortedDict)}.difference([
             "__class__", "__dir__", "__doc__", "__eq__", "__format__", "__ge__", "__getattribute__", "__getitem__",
             "__getstate__", "__gt__", "__hash__", "__init__", "__init_subclass__", "__le__", "__len__", "__lt__",
-            "__ne__", "__new__", "__reduce__", "__reduce_ex__", "__repr__", "__setattr__", "__setitem__", "__sizeof__",
-            "__str__", "__subclasshook__", "clear", "copy", "items", "key_type", "keys", "values",
+            "__ne__", "__new__", "__reduce__", "__reduce_ex__", "__repr__", "__setattr__", "__sizeof__", "__str__",
+            "__subclasshook__", "clear", "copy", "items", "key_type", "keys", "values",
         ])  # fmt: skip
         for attr in self._rg.choices([*attrs], k=10_000):
             getattr(self, f"_test_{attr}")()
