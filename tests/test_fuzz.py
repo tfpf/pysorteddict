b8052b75d793f76cf5af989f41eeccc5fadf2661
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-10 15:45:22 +0530

    Working tests with `__getitem__` placeholder
    Documenting that this is a working state.

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -50,10 +50,10 @@ class TestFuzz:
         self.is_sorted_dict_new = True
 
         attrs = {*dir(SortedDict)}.difference([
-            "__class__", "__dir__", "__doc__", "__eq__", "__format__", "__ge__", "__getattribute__", "__getitem__",
-            "__getstate__", "__gt__", "__hash__", "__init__", "__init_subclass__", "__le__", "__len__", "__lt__",
-            "__ne__", "__new__", "__reduce__", "__reduce_ex__", "__repr__", "__setattr__", "__sizeof__", "__str__",
-            "__subclasshook__", "clear", "copy", "items", "key_type", "keys", "values",
+            "__class__", "__dir__", "__doc__", "__eq__", "__format__", "__ge__", "__getattribute__", "__getstate__",
+            "__gt__", "__hash__", "__init__", "__init_subclass__", "__le__", "__len__", "__lt__", "__ne__", "__new__",
+            "__reduce__", "__reduce_ex__", "__repr__", "__setattr__", "__sizeof__", "__str__", "__subclasshook__",
+            "clear", "copy", "items", "key_type", "keys", "values",
         ])  # fmt: skip
         for attr in self._rg.choices([*attrs], k=10_000):
             getattr(self, f"_test_{attr}")()
@@ -100,6 +100,9 @@ class TestFuzz:
                 continue
             del self.normal_dict[key]
             del self.sorted_dict[key]
+
+    def _test___getitem__(self):
+        pass
 
     def _test___setitem__(self):
         for key_type in all_types:
