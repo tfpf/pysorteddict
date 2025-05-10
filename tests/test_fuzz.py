24d588976892feb0896e629755e3aa31eadb0a58
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-10 15:54:17 +0530

    Ignore extra attribute see on PyPy 3.10

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -53,7 +53,7 @@ class TestFuzz:
             "__class__", "__dir__", "__doc__", "__eq__", "__format__", "__ge__", "__getattribute__", "__getstate__",
             "__gt__", "__hash__", "__init__", "__init_subclass__", "__le__", "__len__", "__lt__", "__ne__", "__new__",
             "__reduce__", "__reduce_ex__", "__repr__", "__setattr__", "__sizeof__", "__str__", "__subclasshook__",
-            "clear", "copy", "items", "key_type", "keys", "values",
+            "__weakref__", "clear", "copy", "items", "key_type", "keys", "values",
         ])  # fmt: skip
         for attr in self._rg.choices([*attrs], k=10_000):
             getattr(self, f"_test_{attr}")()
