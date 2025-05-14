795220da7358aa089ebf49259566b2d3b0b3c076
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-14 21:08:28 +0530

    Usage file [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,13 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'pysorteddict'
 copyright = '2025, Vishal Pankaj Chandratreya'
 author = 'Vishal Pankaj Chandratreya'
 release = 'v0.4.5'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = ["myst_parser"]
+source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,10 @@
-.. pysorteddict documentation master file, created by
-   sphinx-quickstart on Wed May 14 20:40:47 2025.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+pysorteddict
+============
 
-pysorteddict documentation
-==========================
-
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
+Provides ``SortedDict``, which is a Python sorted dictionary: a Python dictionary in which the keys are always in
+ascending order.
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
+   usage
