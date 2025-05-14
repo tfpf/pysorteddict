d68dab6eb3e8fecd4d5f248946f80264a988f898
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-14 22:22:21 +0530

    Installation section [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
-project = "pysorteddict"
+release = "v0.4.5"
+project = f"pysorteddict {release}"
 copyright = "2025, Vishal Pankaj Chandratreya"
 author = "Vishal Pankaj Chandratreya"
-release = "v0.4.5"
 
 extensions = ["myst_parser"]
 source_suffix = [".md", ".rst"]
@@ -10,4 +10,3 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build"]
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
