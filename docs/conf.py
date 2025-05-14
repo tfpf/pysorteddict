a0d93efb5844ab89618500050a90e03e40684f8c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-14 22:10:24 +0530

    Formatted [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,13 @@
-project = 'pysorteddict'
-copyright = '2025, Vishal Pankaj Chandratreya'
-author = 'Vishal Pankaj Chandratreya'
-release = 'v0.4.5'
+project = "pysorteddict"
+copyright = "2025, Vishal Pankaj Chandratreya"
+author = "Vishal Pankaj Chandratreya"
+release = "v0.4.5"
 
 extensions = ["myst_parser"]
-source_suffix = ['.rst', '.md']
+source_suffix = [".md", ".rst"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
