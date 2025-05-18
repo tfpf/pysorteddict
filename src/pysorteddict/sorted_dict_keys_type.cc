cbe0cd827afc19c5b9de18f3374e4cef3c381011
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-18 23:38:49 +0530

    Implement `__repr__` correctly for `SortedDictKeysType`
    [skip ci]

--- src/pysorteddict/sorted_dict_keys_type.cc
+++ src/pysorteddict/sorted_dict_keys_type.cc
@@ -1,6 +1,8 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <string>
+#include <iterator>
+#include <map>
 
 #include "sorted_dict_type.hh"
 #include "sorted_dict_keys_type.hh"
@@ -11,8 +13,20 @@ void SortedDictKeysType::deinit(void){
 }
 
 PyObject* SortedDictKeysType::repr(void){
+    char const* delimiter = "";
+    char const* actual_delimiter = ", ";
     std::string this_repr_utf8 = "SortedDictKeys" LEFT_PARENTHESIS LEFT_CURLY_BRACKET;
-    for (auto& item : *this->sd->map){}
+    for (auto& item : *this->sd->map){
+        PyObjectWrapper key_repr(PyObject_Repr(item.first));  // 🆕
+        if (key_repr == nullptr)
+        {
+            return nullptr;
+        }
+        Py_ssize_t key_repr_size;
+        char const* key_repr_utf8 = PyUnicode_AsUTF8AndSize(key_repr.get(), &key_repr_size);
+        this_repr_utf8.append(delimiter).append(key_repr_utf8, key_repr_size);
+        delimiter = actual_delimiter;
+    }
     this_repr_utf8.append(RIGHT_CURLY_BRACKET RIGHT_PARENTHESIS);
     return PyUnicode_FromStringAndSize(this_repr_utf8.data(), this_repr_utf8.size());  // 🆕
 }
