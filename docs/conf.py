cd9bea719cf47398d956c8ff3ddebee0b57a27c6
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-23 00:42:10 +0530

    Simplify Sphinx configuration [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,14 @@
+import re
+
 with open("../pyproject.toml") as reader:
-    for line in reader:
-        if line.startswith("version"):
-            release = line.split('"')[1]
-            break
-html_title = project = f"pysorteddict {release}"
+    release = re.search(r'^version = "(.*?)"$', reader.read(), flags=re.MULTILINE).group(1)
+project = f"pysorteddict {release}"
 copyright = "2025, Vishal Pankaj Chandratreya"
 author = "Vishal Pankaj Chandratreya"
 
 extensions = ["myst_parser"]
 source_suffix = [".md", ".rst"]
 
-templates_path = ["_templates"]
 exclude_patterns = ["_build"]
 
 html_logo = "logo.svg"
@@ -20,4 +18,5 @@ html_theme_options = {
     "source_directory": "docs",
     "source_repository": "https://github.com/tfpf/pysorteddict",
 }
+html_title = project
 html_use_index = False
