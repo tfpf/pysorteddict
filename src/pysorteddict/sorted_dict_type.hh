e56de0a869e4bae4fa66ced94d7e6cb4eeeeb1cd
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-05-31 19:03:41 +0530

    Reduce fragmentation: move `SortedDictKeyCompare` to `sorted_dict_type.hh`

--- src/pysorteddict/sorted_dict_type.hh
+++ src/pysorteddict/sorted_dict_type.hh
@@ -5,7 +5,23 @@
 #include <Python.h>
 #include <map>
 
-#include "sorted_dict_key_compare.hh"
+/**
+ * C++-style comparison implementation for Python objects.
+ */
+struct SortedDictKeyCompare
+{
+    bool operator()(PyObject* a, PyObject* b) const
+    {
+        // There must exist a total order on the set of possible keys. (Else,
+        // this comparison may error out.) Hence, only instances of the type
+        // of the first key inserted may be used as keys. (Instances of types
+        // derived from that type are not allowed, because comparisons between
+        // them can be customised to error out. Check the code to see how this
+        // is enforced.) With these precautions, this comparison should always
+        // work.
+        return PyObject_RichCompareBool(a, b, Py_LT) == 1;
+    }
+};
 
 struct SortedDictType
 {
