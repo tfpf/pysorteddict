73f6f35f7817fafcb835699575ec8f0a8f60e82c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-05 15:15:17 +0530

    More extensive and precise testing [skip ci]

--- tests/test_keys_iter.py
+++ tests/test_keys_iter.py
@@ -16,23 +16,38 @@ def sorted_dict(request):
 
 @pytest.mark.parametrize("sorted_dict", [0, 1, 10, 100, 1_000, 10_000, 100_000], indirect=True)
 @pytest.mark.parametrize("iterators", range(1, 10))
-@pytest.mark.parametrize("advance", [0, 1, 2, 4, 8, 16])
-def test_setitem_when_iterators_active(sorted_dict, iterators, advance):
+@pytest.mark.parametrize("advance", [0, 1, 2, 4, 8, 16, float("inf")])
+def test_modify_with_active_iterators(sorted_dict, iterators, advance):
     sorted_dict_len = len(sorted_dict)
     sorted_dict_keys = sorted_dict.keys()
     sorted_dict_keys_iters = [iter(sorted_dict_keys) for _ in range(iterators)]
     advances = [random.randint(0, min(advance, sorted_dict_len)) for _ in sorted_dict_keys_iters]
+
+    # Advance the iterators by the arbitrary amounts determined above. Record
+    # the keys the iterators point to as locked keys.
+    keys = [*sorted_dict_keys]
+    locked_keys = collections.defaultdict(int)
     for sorted_dict_keys_iter, advance in zip(sorted_dict_keys_iters, advances):
         [*itertools.islice(sorted_dict_keys_iter, advance)]
-    keys = [*sorted_dict_keys]
-    counter = collections.Counter(advances)
-    for advance in advances:
         if advance < sorted_dict_len:
-            with pytest.raises(RuntimeError, match=re.escape(f"operation not permitted: key-value pair locked by {counter[advance]} iterator(s)")):
-                del sorted_dict[keys[advance]]
-    if count := (iterators - counter[sorted_dict_len]):
-        with pytest.raises(RuntimeError, match=re.escape(f"operation not permitted: sorted dictionary locked by {count} iterator(s)")):
+            locked_keys[keys[advance]] += 1
+
+    sorted_dict_copy = sorted_dict.copy()
+    for locked_key, known_referrers in locked_keys.items():
+        with pytest.raises(RuntimeError, match=re.escape(f"operation not permitted: key-value pair locked by {known_referrers} iterator(s)")):
+            del sorted_dict[locked_key]
+        del sorted_dict_copy[locked_key]
+    for unlocked_key in itertools.islice({*keys} - locked_keys.keys(), 100):
+        del sorted_dict[unlocked_key]
+    if total_known_referrers := sum(locked_keys.values()):
+        with pytest.raises(RuntimeError, match=re.escape(f"operation not permitted: sorted dictionary locked by {total_known_referrers} iterator(s)")):
             sorted_dict.clear()
+    sorted_dict_copy.clear()
+
+    del sorted_dict_keys_iters
+    for locked_key, known_referrers in locked_keys.items():
+        del sorted_dict[locked_key]
+
 
 
 if __name__ == "__main__":
