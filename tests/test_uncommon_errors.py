1f6b7a50a31cf6d8b2af55ef616725618835694e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-09 22:16:01 +0530

    Reduce match string [skip ci]

--- tests/test_uncommon_errors.py
+++ tests/test_uncommon_errors.py
@@ -14,9 +14,7 @@ def test_key_repr_error(sorted_dict):
     sorted_dict[2**15000] = 0
     with pytest.raises(
         ValueError,
-        match=re.escape(
-            "Exceeds the limit (4300 digits) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit"
-        ),
+        match=re.escape("Exceeds the limit (4300 digits) for integer string conversion"),
     ):
         str(sorted_dict)
 
@@ -25,8 +23,6 @@ def test_value_repr_error(sorted_dict):
     sorted_dict[0] = 2**15000
     with pytest.raises(
         ValueError,
-        match=re.escape(
-            "Exceeds the limit (4300 digits) for integer string conversion; use sys.set_int_max_str_digits() to increase the limit"
-        ),
+        match=re.escape("Exceeds the limit (4300 digits) for integer string conversion"),
     ):
         str(sorted_dict)
