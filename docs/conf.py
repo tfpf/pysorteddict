459cc8eabd5e7fe200a792dd48e6020777f44182
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-11 00:28:21 +0530

    Working example

--- docs/conf.py
+++ docs/conf.py
@@ -10,15 +10,15 @@ extensions = ["myst_parser"]
 source_suffix = [".md", ".rst"]
 exclude_patterns = ["_build"]
 
-html_css_files = ["custom.css"]
+html_css_files = ["css/custom.css"]
 html_favicon = "favicon.svg"
 html_logo = "logo.svg"
 html_static_path = ["_static"]
 html_theme = "furo"
 html_theme_options = {
     "light_css_variables": {
-        "font-stack": "Edu NSW ACT Hand Pre, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji",
-        "font-stack--headings": "Edu NSW ACT Hand Pre, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji",
+        "font-stack": "Livvic, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji",
+        "font-stack--headings": "Livvic, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji",
         "font-stack--monospace": "Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
     },
     "source_branch": "main",
@@ -28,4 +28,3 @@ html_theme_options = {
 html_title = project
 html_use_index = False
 myst_heading_anchors = 3
-templates_path = ['_templates']
