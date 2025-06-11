defe5b3f19f65776f65b6b3737b044fbeaf903e9
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-11 21:17:47 +0530

    Remove changes not related reorganising website

--- docs/conf.py
+++ docs/conf.py
@@ -16,15 +16,11 @@ html_logo = "_static/images/logo.svg"
 html_static_path = ["_static"]
 html_theme = "furo"
 html_theme_options = {
-    "light_css_variables": {
-        "font-stack": "Livvic, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji",
-        "font-stack--headings": "Livvic, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji",
-        "font-stack--monospace": "Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
-    },
     "source_branch": "main",
     "source_directory": "docs",
     "source_repository": "https://github.com/tfpf/pysorteddict",
 }
 html_title = project
 html_use_index = False
+
 myst_heading_anchors = 3
