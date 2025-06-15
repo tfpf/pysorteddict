f4b006afa4453c5395c2d1594b732e44ab7e2e87
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-15 17:13:55 +0530

    Fix error message to match

--- tests/test_uncommon_errors.py
+++ tests/test_uncommon_errors.py
@@ -8,12 +8,12 @@ from pysorteddict import SortedDict
 def test_key_repr_error():
     sorted_dict = SortedDict()
     sorted_dict[2**15000] = 0
-    with pytest.raises(ValueError, match=re.escape("Exceeds the limit (4300 digits) for integer string conversion")):
+    with pytest.raises(ValueError, match=re.escape("Exceeds the limit")):
         str(sorted_dict)
 
 
 def test_value_repr_error():
     sorted_dict = SortedDict()
     sorted_dict[0] = 2**15000
-    with pytest.raises(ValueError, match=re.escape("Exceeds the limit (4300 digits) for integer string conversion")):
+    with pytest.raises(ValueError, match=re.escape("Exceeds the limit")):
         str(sorted_dict)
