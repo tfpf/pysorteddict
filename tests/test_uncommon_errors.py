8b7cee06a93e43e8ceabe297a97fe7cf4bd7f725
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-15 18:29:31 +0530

    Increase test coverage (#142)
    Test stringification errors.

---
+++ tests/test_uncommon_errors.py
@@ -0,0 +1,19 @@
+import re
+
+import pytest
+
+from pysorteddict import SortedDict
+
+
+def test_key_repr_error():
+    sorted_dict = SortedDict()
+    sorted_dict[2**15000] = 0
+    with pytest.raises(ValueError, match=re.escape("Exceeds the limit")):
+        str(sorted_dict)
+
+
+def test_value_repr_error():
+    sorted_dict = SortedDict()
+    sorted_dict[0] = 2**15000
+    with pytest.raises(ValueError, match=re.escape("Exceeds the limit")):
+        str(sorted_dict)
