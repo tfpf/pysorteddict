ccd44a65c7c8e0cbe022aac4604e4a4484feb260
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-17 22:30:01 +0530

    Parse `pyproject.toml` to obtain data for website
    [skip ci]

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-import glob
 import os
 import sysconfig
+from glob import glob
 
-import setuptools
+from setuptools import Extension, setup
 
 # MSVC supports designated initialisers only in C++20.
 cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++20"
 
-setuptools.setup(
-    ext_modules=[
-        setuptools.Extension("pysorteddict", glob.glob("src/pysorteddict/*.cc"), extra_compile_args=[cxx_standard_arg])
-    ],
+setup(
+    ext_modules=[Extension("pysorteddict", glob("src/pysorteddict/*.cc"), extra_compile_args=[cxx_standard_arg])],
     include_package_data=False,
 )
