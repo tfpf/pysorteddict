f04935acc853df7b3edd2dd3218c602d975a5026
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-18 22:05:06 +0530

    Restore `setup.py` [skip ci]

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+import glob
 import os
 import sysconfig
-from glob import glob
 
-from setuptools import Extension, setup
+import setuptools
 
 # MSVC supports designated initialisers only in C++20.
 cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++20"
 
-setup(
-    ext_modules=[Extension("pysorteddict", glob("src/pysorteddict/*.cc"), extra_compile_args=[cxx_standard_arg])],
+setuptools.setup(
+    ext_modules=[
+        setuptools.Extension("pysorteddict", glob.glob("src/pysorteddict/*.cc"), extra_compile_args=[cxx_standard_arg])
+    ],
     include_package_data=False,
 )
