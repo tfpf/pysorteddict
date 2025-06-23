105af8ec480ebeba9fd3a16c47bc40b4997d1c69
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-23 23:25:21 +0530

    Compiler and linker arguments

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-import os
 import sysconfig
 from glob import glob
 
 from setuptools import Extension, setup
 
-# Designated initialisers were added in C++20.
-cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++20"
-
-setup(ext_modules=[Extension("pysorteddict", glob("src/pysorteddict/*.cc"), extra_compile_args=[cxx_standard_arg])])
+# Only non-static (i.e. platform-specific) configurations go here.
+cc = sysconfig.get_config_var("CC")
+if cc and "msvc" in cc:
+    extra_compile_args = ["/GL", "/std:c++20"]
+    extra_link_args = ["/LTCG"]
+else:
+    extra_compile_args = ["-flto", "-std=c++20"]
+    extra_link_args = ["-flto"]
+setup(
+    ext_modules=[
+        Extension(
+            "pysorteddict",
+            glob("src/pysorteddict/*.cc"),
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
+        )
+    ]
+)
