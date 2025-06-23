d05fed462f9289b4e29c4cb01ae5fa793ddecbcc
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-23 23:05:59 +0530

    Move a Setuptools configuration to `pyproject.toml` [skip ci]

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ import sysconfig
 
 import setuptools
 
-# MSVC supports designated initialisers only in C++20.
+# Designated initialisers were added in C++20.
 cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++20"
 
 setuptools.setup(
     ext_modules=[
         setuptools.Extension("pysorteddict", glob.glob("src/pysorteddict/*.cc"), extra_compile_args=[cxx_standard_arg])
-    ],
-    include_package_data=False,
+    ]
 )
