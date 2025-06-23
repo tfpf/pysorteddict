367e9d0e418234167446b3b0b0e3c174d566de8d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-23 23:36:13 +0530

    Restored MSVC check

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
+import os
 import sysconfig
 from glob import glob
 
 from setuptools import Extension, setup
 
 # Only non-static (i.e. platform-specific) configurations go here.
-cc = sysconfig.get_config_var("CC")
-if cc and "msvc" in cc:
-    extra_compile_args = ["/GL", "/std:c++20"]
-    extra_link_args = ["/LTCG"]
-else:
+if os.name != "nt" or sysconfig.get_platform().startswith("mingw"):
     extra_compile_args = ["-flto", "-std=c++20"]
     extra_link_args = ["-flto"]
+else:
+    extra_compile_args = ["/GL", "/std:c++20"]
+    extra_link_args = ["/LTCG"]
 setup(
     ext_modules=[
         Extension(
