7a6f2a87766bf3303f9abb184dbef3fe73031bed
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-25 23:27:25 +0530

    Remove unnecessary use of `re.escape` [skip ci]
    Closes #159.

--- tests/test_uncommon_errors.py
+++ tests/test_uncommon_errors.py
@@ -1,5 +1,3 @@
-import re
-
 import pytest
 
 from pysorteddict import SortedDict
@@ -8,12 +6,12 @@ from pysorteddict import SortedDict
 def test_key_repr_error():
     sorted_dict = SortedDict()
     sorted_dict[2**15000] = 0
-    with pytest.raises(ValueError, match=re.escape("Exceeds the limit")):
+    with pytest.raises(ValueError, match="Exceeds the limit"):
         str(sorted_dict)
 
 
 def test_value_repr_error():
     sorted_dict = SortedDict()
     sorted_dict[0] = 2**15000
-    with pytest.raises(ValueError, match=re.escape("Exceeds the limit")):
+    with pytest.raises(ValueError, match="Exceeds the limit"):
         str(sorted_dict)
