4b5755632b75486a152997d1963222510d1657e8
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-06-27 20:05:25 +0530

    WIP [skip ci]

--- src/pysorteddict/sorted_dict_keys_type.hh
+++ src/pysorteddict/sorted_dict_keys_type.hh
@@ -14,13 +14,15 @@ public:
 
 struct SortedDictKeysType : public SortedDictViewType
 {
+public:
+    using SortedDictViewType::getitem;
+
 private:
     PyObject* getitem(Py_ssize_t);
     PyObject* getitem(Py_ssize_t, Py_ssize_t, Py_ssize_t, Py_ssize_t);
 
 public:
     int contains(PyObject*);
-    using SortedDictViewType::getitem;
 };
 
 #endif
