e377ffa01feea86e53b1139a0a0d4bf284d92f22
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-10-03 00:50:37 +0530

    Experiment

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import glob
-import platform
+import os
 import sysconfig
 
 import setuptools
 
-if (system := platform.system()) == "Darwin":
-    # Clang doesn't know how to resolve template specialisations when explicit
-    # instantiations are provided.
-    sysconfig.get_config_vars().update({"CC": "gcc-12", "CXX": "g++-12"})
-
 # Only non-static (i.e. platform-specific) configurations go here.
-if system != "Windows" or sysconfig.get_platform().startswith("mingw"):
+if os.name != "nt" or sysconfig.get_platform().startswith("mingw"):
     extra_compile_args = ["-flto", "-std=c++20"]
     extra_link_args = ["-flto"]
 else:
