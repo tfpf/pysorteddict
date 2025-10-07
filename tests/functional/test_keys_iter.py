9f9963af4bfc309c61e1adb5c639fbd065950fa0
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-10-07 21:27:27 +0530

    Destructive iteration test

--- tests/functional/test_keys_iter.py
+++ tests/functional/test_keys_iter.py
@@ -65,3 +65,11 @@ def test_modify_with_active_iterators(sorted_dict, iterators, advance):
         ):
             sorted_dict.clear()
     sorted_dict_copy.clear()
+
+
+@pytest.mark.parametrize("sorted_dict", [*range(10), 100, 1_000, 10_000, 100_000], indirect=True)
+def test_destructive_forward_iteration(sorted_dict):
+    for key in sorted_dict:
+        del sorted_dict[key]
+    assert len(sorted_dict) == 0
+    assert not [*sorted_dict]
