9bbf288b01af437da81a7243fcdb2464741a6073
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-11-30 22:20:48 +0530

    Revert "Remove extra compiler and linker arguments"
    This reverts commit 19ca812926bb61e0304fbdf6e7f5d2e88845401e.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 import glob
+import os
+import sysconfig
 
 import setuptools
 
+# Only non-static (i.e. platform-specific) configurations go here.
+if os.name != "nt" or sysconfig.get_platform().startswith("mingw"):
+    extra_compile_args = ["-flto", "-std=c++20"]
+    extra_link_args = ["-flto"]
+else:
+    extra_compile_args = ["/GL", "/std:c++20"]
+    extra_link_args = ["/LTCG"]
 setuptools.setup(
     ext_modules=[
         setuptools.Extension(
             "pysorteddict",
             glob.glob("src/pysorteddict/*.cc"),
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
         )
     ]
 )
