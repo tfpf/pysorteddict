69e8c7de7a7f7d5415dbc83bc380add2056a4ef3
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-01-27 16:37:37 +0530

    Access instance attributes for fine-grained scenario control
    [skip ci]

--- tests/functional/test_fuzz.py
+++ tests/functional/test_fuzz.py
@@ -1,42 +1,50 @@
 import re
 import string
+from datetime import date, timedelta
 from decimal import Decimal
-from ipaddress import IPv4Interface, IPv4Network, IPv6Interface, IPv6Network
+from fractions import Fraction
+from ipaddress import IPv4Address, IPv4Interface, IPv4Network, IPv6Address, IPv6Interface, IPv6Network
 from pathlib import Path, PurePath
+from uuid import UUID
 
 import pytest
 from hypothesis import settings
 from hypothesis import strategies as st
-from hypothesis.stateful import Bundle, RuleBasedStateMachine, invariant, precondition, rule
+from hypothesis.stateful import RuleBasedStateMachine, invariant, precondition, rule
 
 from pysorteddict import SortedDict
 
 settings.register_profile("default", max_examples=200, stateful_step_count=100)
-supported_keys = st.one_of(
-    st.booleans(),
-    st.binary(),
-    st.floats(allow_nan=False),
-    st.integers(),
-    st.text(),
-    st.dates(),
-    st.timedeltas(),
-    st.decimals(allow_nan=False),
-    st.fractions(),
-    st.ip_addresses(v=4),
-    st.from_type(IPv4Interface),
-    st.from_type(IPv4Network),
-    st.ip_addresses(v=6),
-    st.from_type(IPv6Interface),
-    st.from_type(IPv6Network),
-    st.builds(Path, st.text(alphabet=string.ascii_lowercase + "/")),
-    st.builds(PurePath, st.text(alphabet=string.ascii_lowercase + "/")),
+
+strategy_mapping = {
+    bool: st.booleans(),
+    bytes: st.binary(),
+    float: st.floats(allow_nan=False),
+    int: st.integers(),
+    str: st.text(),
+    date: st.dates(),
+    timedelta: st.timedeltas(),
+    Decimal: st.decimals(allow_nan=False),
+    Fraction: st.fractions(),
+    IPv4Address: st.ip_addresses(v=4),
+    IPv4Interface: st.from_type(IPv4Interface),
+    IPv4Network: st.from_type(IPv4Network),
+    IPv6Address: st.ip_addresses(v=6),
+    IPv6Interface: st.from_type(IPv6Interface),
+    IPv6Network: st.from_type(IPv6Network),
+    type(Path()): st.builds(Path, st.text(alphabet=string.ascii_lowercase + "/")),
+    type(PurePath()): st.builds(PurePath, st.text(alphabet=string.ascii_lowercase + "/")),
     # The representation of objects of this type in Python is different from
     # that when using the C++ API. I must resolve that difference first.
     # st.builds(time.localtime, st.integers(min_value=0, max_value=2**30)),
-    st.uuids(),
-)
-unsupported_keys = st.lists(st.integers()) | st.tuples(st.integers())
-unsupported_supported_keys = unsupported_keys | supported_keys
+    UUID: st.uuids(),
+}
+strategy_mapping_complement = {
+    tp: st.one_of(other_strat for other_strat in strategy_mapping.values() if other_strat != strat)
+    for tp, strat in strategy_mapping.items()
+}
+all_keys = (supported_keys := st.one_of(strategy_mapping.values())) | (unsupported_keys := st.lists(st.integers()))
+strategy_mapping[None] = supported_keys
 
 
 def prec_key_type_not_set(self):
@@ -55,14 +63,9 @@ class SortedDictionaryChecker(RuleBasedStateMachine):
     def __init__(self, sorted_dict_type=SortedDict):
         super().__init__()
         self.key_type = None
+        self.keys = []
         self.normal_dict = {}
         self.sorted_dict = sorted_dict_type()
-
-    keys = Bundle("keys")
-
-    @rule(target=keys, key=supported_keys)
-    def add_key(self, key):
-        return key
 
     @invariant()
     def always(self):
@@ -77,21 +80,18 @@ class SortedDictionaryChecker(RuleBasedStateMachine):
     ###########################################################################
 
     @precondition(prec_key_type_not_set)
-    @rule(key=unsupported_supported_keys)
+    @rule(key=all_keys)
     def contains_key_type_not_set(self, key):
         with pytest.raises(RuntimeError, match="key type not set: insert at least one item first"):
             _ = key in self.sorted_dict
 
     @precondition(prec_key_type_set)
-    @rule(key=unsupported_supported_keys | keys)
+    @rule(key=st.runner().flatmap(lambda self: strategy_mapping_complement[self.key_type]))
     def contains_wrong_type(self, key):
-        if type(key) is not self.key_type:
-            with pytest.raises(
-                TypeError, match=re.escape(f"got key {key!r} of type {type(key)}, want key of type {self.key_type}")
-            ):
-                _ = key in self.sorted_dict
-        else:
-            assert (key in self.sorted_dict) == (key in self.normal_dict)
+        with pytest.raises(
+            TypeError, match=re.escape(f"got key {key!r} of type {type(key)}, want key of type {self.key_type}")
+        ):
+            _ = key in self.sorted_dict
 
     @precondition(prec_key_type_admits_nan)
     @rule()
@@ -100,29 +100,39 @@ class SortedDictionaryChecker(RuleBasedStateMachine):
         with pytest.raises(ValueError, match=re.escape(f"got bad key {key!r} of type {type(key)}")):
             _ = key in self.sorted_dict
 
-    ###########################################################################
-    # `getitem` tests.
-    ###########################################################################
-
-    @precondition(prec_key_type_not_set)
-    @rule(key=unsupported_supported_keys)
-    def getitem_key_type_not_set(self, key):
-        with pytest.raises(RuntimeError, match="key type not set: insert at least one item first"):
-            self.sorted_dict[key]
+    @precondition(prec_key_type_set)
+    @rule(key=st.runner().flatmap(lambda self: strategy_mapping[self.key_type]))
+    def contains_probably_false(self, key):
+        assert (key in self.sorted_dict) == (key in self.normal_dict)
 
     @precondition(prec_key_type_set)
-    @rule(key=unsupported_supported_keys | keys)
-    def getitem_wrong_type_key_error(self, key):
-        if type(key) is not self.key_type:
-            with pytest.raises(
-                TypeError, match=re.escape(f"got key {key!r} of type {type(key)}, want key of type {self.key_type}")
-            ):
-                self.sorted_dict[key]
-        elif key not in self.normal_dict:
-            with pytest.raises(KeyError, match=re.escape(f"{key!r}")):
-                self.sorted_dict[key]
-        else:
-            assert self.sorted_dict[key] == self.normal_dict[key]
+    @rule(key=st.runner().flatmap(lambda self: st.sampled_from(self.keys)))
+    def contains_true(self, key):
+        assert key in self.sorted_dict
+
+    # ###########################################################################
+    # # `getitem` tests.
+    # ###########################################################################
+
+    # @precondition(prec_key_type_not_set)
+    # @rule(key=all_keys)
+    # def getitem_key_type_not_set(self, key):
+    #     with pytest.raises(RuntimeError, match="key type not set: insert at least one item first"):
+    #         self.sorted_dict[key]
+
+    # @precondition(prec_key_type_set)
+    # @rule(key=all_keys | keys)
+    # def getitem_wrong_type_key_error(self, key):
+    #     if type(key) is not self.key_type:
+    #         with pytest.raises(
+    #             TypeError, match=re.escape(f"got key {key!r} of type {type(key)}, want key of type {self.key_type}")
+    #         ):
+    #             self.sorted_dict[key]
+    #     elif key not in self.normal_dict:
+    #         with pytest.raises(KeyError, match=re.escape(f"{key!r}")):
+    #             self.sorted_dict[key]
+    #     else:
+    #         assert self.sorted_dict[key] == self.normal_dict[key]
 
     ###########################################################################
     # `setitem` tests.
@@ -141,16 +151,12 @@ class SortedDictionaryChecker(RuleBasedStateMachine):
             self.sorted_dict[key] = None
 
     @precondition(prec_key_type_set)
-    @rule(key=unsupported_supported_keys | keys, value=st.integers())
-    def setitem_wrong_type(self, key, value):
-        if type(key) is not self.key_type:
-            with pytest.raises(
-                TypeError, match=re.escape(f"got key {key!r} of type {type(key)}, want key of type {self.key_type}")
-            ):
-                self.sorted_dict[key] = None
-        else:
-            self.normal_dict[key] = value
-            self.sorted_dict[key] = value
+    @rule(key=st.runner().flatmap(lambda self: strategy_mapping_complement[self.key_type]))
+    def setitem_wrong_type(self, key):
+        with pytest.raises(
+            TypeError, match=re.escape(f"got key {key!r} of type {type(key)}, want key of type {self.key_type}")
+        ):
+            self.sorted_dict[key] = None
 
     @precondition(prec_key_type_admits_nan)
     @rule()
@@ -163,6 +169,21 @@ class SortedDictionaryChecker(RuleBasedStateMachine):
     @rule(key=supported_keys, value=st.integers())
     def setitem_empty(self, key, value):
         self.key_type = type(key)
+        self.keys.append(key)
+        self.normal_dict[key] = value
+        self.sorted_dict[key] = value
+
+    @precondition(prec_key_type_set)
+    @rule(key=st.runner().flatmap(lambda self: strategy_mapping[self.key_type]), value=st.integers())
+    def setitem_probably_new(self, key, value):
+        if key not in self.normal_dict:
+            self.keys.append(key)
+        self.normal_dict[key] = value
+        self.sorted_dict[key] = value
+
+    @precondition(prec_key_type_set)
+    @rule(key=st.runner().flatmap(lambda self: st.sampled_from(self.keys)), value=st.integers())
+    def setitem_existing(self, key, value):
         self.normal_dict[key] = value
         self.sorted_dict[key] = value
 
