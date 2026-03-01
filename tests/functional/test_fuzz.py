c68ffd05e5545fa5807ebe9df8fdc042b1af06bc
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-03-01 20:37:40 +0530

    Fix incorrect testing of edge case

--- tests/functional/test_fuzz.py
+++ tests/functional/test_fuzz.py
@@ -200,13 +200,13 @@ class IteratorWrapper:
 
     def next_fwd(self):
         # Forward iterators yield the locked key and then lock the next key.
-        correct_key = self.locked_key
+        next_key = self.locked_key
         observed = next(self.iterator)
         try:
             self.locked_key = min(key for key in self.keys if key > self.locked_key)
         except ValueError:
             self.active = False
-        return correct_key, observed
+        return next_key, observed
 
     def next_rev(self):
         # Reverse iterators lock the key they just yielded. The difference is
@@ -221,12 +221,12 @@ class IteratorWrapper:
                 # This edge case is tested in another file.
                 self.active = False
                 return None, None
-        correct_key = self.locked_key
+        next_key = self.locked_key
         observed = next(self.iterator)
         if self.locked_key == min(self.keys):
             # All keys have been yielded.
             self.active = False
-        return correct_key, observed
+        return next_key, observed
 
 
 class FuzzMachine(RuleBasedStateMachine):
@@ -554,9 +554,13 @@ class FuzzMachine(RuleBasedStateMachine):
     @precondition(prec_active_iterators_not_empty)
     @rule(iterator=rule_active_iterator())
     def next_active(self, iterator):
-        correct_key, observed = iterator.next()
-        expected = self.key_to_item_or_key_or_value(correct_key, iterator.iterator)
-        assert observed == expected
+        next_key, observed = iterator.next()
+        if next_key is None:
+            with pytest.raises(StopIteration):
+                next(iterator.iterator)
+        else:
+            expected = self.key_to_item_or_key_or_value(next_key, iterator.iterator)
+            assert observed == expected
 
     @precondition(prec_inactive_iterators_not_empty)
     @rule(iterator=rule_inactive_iterator())
