288d3b0d4baed87f08edb903d21816ed57dde2d0
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-03-01 13:52:39 +0530

    Test edge case

--- tests/functional/test_uncommon_errors.py
+++ tests/functional/test_uncommon_errors.py
@@ -15,3 +15,14 @@ def test_value_repr_error():
     sorted_dict[0] = 2**15000
     with pytest.raises(ValueError, match="Exceeds the limit"):
         str(sorted_dict)
+
+
+def test_reverse_iterator_yield_after_removing_elements():
+    sorted_dict = SortedDict()
+    sorted_dict[0] = 0
+    sorted_dict[1] = 1
+    r = reversed(sorted_dict)
+    assert next(r) == 1
+    del sorted_dict[0]
+    with pytest.raises(StopIteration):
+        next(r)
