94c1d33c40e017cb25a26084d1c5241dae2be120
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-04-28 20:52:29 +0530

    Move C++ formatter

---
+++ scripts/clang-format.bash
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+shopt -s globstar
+
+files=("${0%/*}"/../src/**/*.{cc,hh})
+if [ "$1" = gha ]
+then
+    clang-format --dry-run --verbose --Werror "${files[@]}"
+else
+    clang-format -i --verbose "${files[@]}"
+fi
