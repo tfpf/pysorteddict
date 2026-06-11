909ddd19cfedc537ee5e11cc1b8e391be7124140
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-06-11 23:48:44 +0530

    Hatch lints [skip ci]

--- docs/_ext/details.py
+++ docs/_ext/details.py
@@ -1,13 +1,16 @@
+from typing import ClassVar
+
 from docutils import nodes
 from docutils.parsers.rst import Directive, directives
-
 from sphinx.application import Sphinx
 from sphinx.util.typing import ExtensionMetadata
 
-class details(nodes.General, nodes.Element):
+
+class DetailsNode(nodes.General, nodes.Element):
     pass
 
-def visit_details(self, node: details):
+
+def visit_details(self, node: DetailsNode):
     details_attributes = []
     if classes := node["class"]:
         details_attributes.append('class="' + " ".join(classes) + '"')
@@ -16,29 +19,31 @@ def visit_details(self, node: details):
     self.body.append("<details " + " ".join(details_attributes) + ">")
     self.body.append("<summary>" + node["summary"] + "</summary>")
 
-def depart_details(self, node: details):
+
+def depart_details(self, _node: DetailsNode):
     self.body.append("</details>")
 
+
 class DetailsDirective(Directive):
-    final_argument_whitespace = True
-    has_content = True
-    option_spec = {"class": directives.class_option, "open": directives.flag}
-    required_arguments = 1
+    final_argument_whitespace: bool = True
+    has_content: bool = True
+    option_spec: ClassVar[dict] = {"class": directives.class_option, "open": directives.flag}
+    required_arguments: int = 1
 
     def run(self):
-        node = details()
+        node = DetailsNode()
         node["class"] = self.options.get("class")
         node["open"] = "open" in self.options
         node["summary"] = self.arguments[0]
         self.state.nested_parse(self.content, self.content_offset, node)
         return [node]
 
+
 def setup(app: Sphinx) -> ExtensionMetadata:
-    app.add_node(details, html=(visit_details, depart_details))
+    app.add_node(DetailsNode, html=(visit_details, depart_details))
     app.add_directive("details", DetailsDirective)
     return {
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
-        'version': '0.0',
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+        "version": "0.0",
     }
-
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
+import sys
 import time
 import zipfile
-import sys
 from pathlib import Path
 
 import tomllib
