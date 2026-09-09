325f9aa683a9137a0b4bef07c77981c4eba9a23b
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-09-10 00:47:29 +0530

    Fix null dereference [skip ci]

--- src/pysorteddict/sorted_dict_type.cc
+++ src/pysorteddict/sorted_dict_type.cc
@@ -304,7 +304,7 @@ bool SortedDictType::update_from_sorted_dict(PyObject* sd)
         PyObject* key = item.first;
         PyObject* value = item.second.value;
         auto it = this->map->lower_bound(key);
-        if (this->map->key_comp()(key, it->first))
+        if (it == this->map->end() || this->map->key_comp()(key, it->first))
         {
             this->map->emplace_hint(it, Py_NewRef(key), value);  // 🆕
         }
@@ -313,7 +313,7 @@ bool SortedDictType::update_from_sorted_dict(PyObject* sd)
             Py_DECREF(it->second.value);
             it->second.value = value;
         }
-        Py_INCREF(it->second.value);  // 🆕
+        Py_INCREF(value);  // 🆕
     }
     if (this->key_type == nullptr && !this->map->empty())
     {
